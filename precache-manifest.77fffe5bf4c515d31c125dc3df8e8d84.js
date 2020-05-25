3733fde94ae768a8d015f9a35ad8929a371232f9
mtibo62 <mark@DESKTOP-21O1F6B.localdomain>
2020-05-25 15:25:28 -0500

    Updates

---
+++ precache-manifest.77fffe5bf4c515d31c125dc3df8e8d84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e224d851a5b3ec5cb3e1127bb1e5737",
+    "url": "/PortfolioSite/index.html"
+  },
+  {
+    "revision": "eb24629cb810f150f322",
+    "url": "/PortfolioSite/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "260e31633297527bcdb0",
+    "url": "/PortfolioSite/static/js/2.538e2618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PortfolioSite/static/js/2.538e2618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb24629cb810f150f322",
+    "url": "/PortfolioSite/static/js/main.5a0c2be0.chunk.js"
+  },
+  {
+    "revision": "e5cb229199655fe28fb2",
+    "url": "/PortfolioSite/static/js/runtime-main.d8ca8381.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PortfolioSite/static/media/logo.5d5d9eef.svg"
+  }
+]);
